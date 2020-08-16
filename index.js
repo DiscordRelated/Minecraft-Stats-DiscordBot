098f833b33ddc62b7fe486ad660e12eefd357212
Tim <69757707+Tim2906@users.noreply.github.com>
2020-08-16 16:05:36 +0200

    .

---
+++ index.js
@@ -0,0 +1,62 @@
+var MC_IP = 'YOUR SERVER IP';
+var MC_PORT = 'YOUR SERVER PORT';
+var EXPRESS_PORT = 'YOUR EXPRESS API PORT';
+var DISCORD_CHANNEL_ID = 'YOUR DISCORD CHANNEL ID';
+var DISCORD_API_KEY = 'YOUR DISCORD BOT API TOKEN';
+var MINECRAFT_SERVER_NAME = 'YOUR MINECRAFT SERVER NAME';
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var tcpp = require('tcp-ping');
+const { SSL_OP_EPHEMERAL_RSA, SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
+var ms = require('./minestat');
+const express = require('express')
+const app = express()
+const port = EXPRESS_PORT;
+
+app.get('/api', (req, res) => {
+  res.send(MINECRAFT_SERVER_NAME + ' slots ' + ms.current_players + " / " + ms.max_players + ' server-ping: ' + ms.latency + 'ms')
+  })
+  
+app.listen(port, () => {
+    console.log('Web listener Online!')
+  })
+
+
+client.on('ready', () =>{
+    console.log("Discord Bot Online!"); 
+})
+
+client.on('message', status =>{
+    if (status.content === '/status'){
+        if (status.channel.id === DISCORD_CHANNEL_ID){
+            tcpp.probe(MC_IP, MC_PORT, function(err, available){
+                if (available){
+                    ms.init(MC_IP, MC_PORT, function(result){
+                        status.channel.send({embed: {
+                            color: 3066993,
+                            title: "Astro-life.eu ist Online",
+                            fields: [{
+                                name: "Spieler Online:",
+                                value: ms.current_players + " / " + ms.max_players
+                              },
+                              {
+                                name: "Server Ping:",
+                                value: ms.latency + "ms"
+                              }
+                            ],
+                            footer: {
+                              text: "Viel Spaß auf unserem Server"
+                            }
+                          }
+                        });
+                    });
+                }else{
+                    status.channel.send("Der Minecraft Server ist derzeit offline!");
+                }
+            });
+        }
+    }
+})
+
+client.login(DISCORD_API_KEY);
---
+++ minestat.js
@@ -0,0 +1,77 @@
+const NUM_FIELDS = 6;      
+const DEFAULT_TIMEOUT = 5; 
+address = null;
+port = null;
+online = null;             
+version = null;            
+motd = null;               
+current_players = null;    
+max_players = null;        
+latency = null;            
+
+module.exports =
+{
+  init: function(address, port, timeout, callback)
+  {
+    this.address = address;
+    this.port = port;
+
+    if(typeof(timeout) === typeof(Function()))
+    {
+      callback = timeout;
+      timeout = DEFAULT_TIMEOUT;
+    }
+
+    const net = require('net');
+    var start_time = new Date();
+    const client = net.connect(port, address, () =>
+    {
+      this.latency = Math.round(new Date() - start_time);
+      var buff = Buffer.from([ 0xFE, 0x01 ]);
+      client.write(buff);
+    });
+
+    client.setTimeout(timeout * 1000);
+
+    client.on('data', (data) =>
+    {
+      if(data != null && data != '')
+      {
+        var server_info = data.toString().split("\x00\x00\x00");
+        if(server_info != null && server_info.length >= NUM_FIELDS)
+        {
+          this.online = true;
+          this.version = server_info[2].replace(/\u0000/g,'');
+          this.motd = server_info[3].replace(/\u0000/g,'');
+          this.current_players = server_info[4].replace(/\u0000/g,'');
+          this.max_players = server_info[5].replace(/\u0000/g,'');
+        }
+        else
+        {
+          this.online = false;
+        }
+      }
+      callback();
+      client.end();
+    });
+
+    client.on('timeout', () =>
+    {
+      callback();
+      client.end();
+      process.exit();
+    });
+
+    client.on('end', () =>
+    {
+
+    });
+
+    client.on('error', (err) =>
+    {
+
+      callback();
+
+    });
+  }
+};
